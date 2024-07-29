83243597c07b3e9e65106c5f8f31a8519577faf7
Talha Hussain <talhahussain.splitarts@gmail.com>
2024-07-29 09:47:49 +0500

    Developed the game ui and fix logic.

--- script.js
+++ script.js
@@ -1,95 +1,60 @@
 function getComputerChoice() {
-  let compChoice = Math.floor(Math.random() * 3 + 1);
-  let str;
-  switch (compChoice) {
-    case 1:
-      str = "Rock";
-      break;
-    case 2:
-      str = "Paper";
-      break;
-    case 3:
-      str = "Scissors";
-      break;
-
-    default:
-      break;
-  }
-  let result = str.toString().toLowerCase();
-  console.log("Computer: "+result);
-  return result;
-}
-
-function getHumanChoice() {
-  console.log("Choose 1 for Rock, 2 for Paper, and 3 for Scissors");
-  let userChoice = parseInt(prompt("Your Choice: "));
-
-  let str;
-  switch (userChoice) {
-    case 1:
-      str = "Rock";
-      break;
-    case 2:
-      str = "Paper";
-      break;
-    case 3:
-      str = "Scissors";
-      break;
-    default:
-      str = "Please select the correct option.";
-      break;
-  }
-
-  let result = str.toString().toLowerCase();
-  console.log("Player: "+result);
+  const choices = ["rock", "paper", "scissors"];
+  const compChoice = Math.floor(Math.random() * 3);
+  const result = choices[compChoice];
   return result;
 }
 
 let humanScore = 0;
 let compScore = 0;
 
+const playerScore = document.querySelector("#score");
+console.log(humanScore);
+playerScore.textContent = `Your score: ${humanScore}\nComp score: ${compScore} `;
+
 function playRound(humanChoice, compChoice) {
-  let result;
-  if (
-    (humanChoice === "rock" && compChoice === "scissors") ||
-    (humanChoice === "scissors" && compChoice === "paper") ||
-    (humanChoice === "paper" && compChoice === "rock")
-  ) {
+  const winningConditions = {
+    rock: "scissors",
+    scissors: "paper",
+    paper: "rock",
+  };
+
+  if (humanChoice === compChoice) {
+    return `It's a tie\n\n`;
+  } else if (winningConditions[humanChoice] === compChoice) {
     humanScore++;
-    result = `You win! ${humanChoice} beats ${compChoice}\n\n`;
-  } else if (
-    (compChoice === "rock" && humanChoice === "scissors") ||
-    (compChoice === "scissors" && humanChoice === "paper") ||
-    (compChoice === "paper" && humanChoice === "rock")
-  ) {
+    return `You win! ${humanChoice} beats ${compChoice}\n\n`;
+  } else if (winningConditions[compChoice] === humanChoice) {
     compScore++;
-    result = `You lose! ${compChoice} beats ${humanChoice}\n\n`;
-  } else if (
-    (humanChoice === "rock" && compChoice === "rock") ||
-    (humanChoice === "scissors" && compChoice === "scissors") ||
-    (humanChoice === "paper" && compChoice === "paper")
-  ) {
-    result = `It's a tie\n\n`;
-  }else{
-    result = `no result\n\n`
+    return `You lose! ${compChoice} beats ${humanChoice}`;
+  } else {
+    return `No result\n\n`;
   }
-  
-  return result;
 }
 
-let winner = function () {
-  for (let i = 0; i < 5; i++) {
-    const humanChoice = getHumanChoice();
-    const compChoice = getComputerChoice();
-    console.log(playRound(humanChoice, compChoice));
-  }
-  if(humanScore > compScore){
-    console.log(`Congragulations! You win. \nYour score ${humanScore}\nOpponent score ${compScore}`);
-  }else if(compScore > humanScore){
-    console.log(`Better luck next time! You lost. \nYour score ${humanScore} \nOpponent score ${compScore}`);
-  }else{
-    console.log(`It's a tie!\nYour score ${humanScore} \nOpponent score ${compScore}`);
-  }
-};
+const btns = document.querySelectorAll("button");
+const gameWinner = document.querySelector("#final-winner");
 
-winner();
+btns.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (humanScore < 5 && compScore < 5) {
+      const humanChoice = e.target.textContent.toLowerCase();
+      const winner = playRound(humanChoice, getComputerChoice());
+      console.log(winner);
+      const winPlayer = document.querySelector("#winner");
+      winPlayer.textContent = winner;
+      playerScore.textContent = `Your score: ${humanScore} | Comp score: ${compScore} `;
+
+      if (humanScore == 5 || compScore == 5) {
+        if (humanScore > compScore) {
+          gameWinner.textContent = `You are the winner`;
+        } else {
+          gameWinner.textContent = `Computer is the winner`;
+        }
+        btns.forEach((button) => {
+          button.disabled = true;
+        });
+      }
+    }
+  });
+});
